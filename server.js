a8d423636b5f9b1e0df0eaf9b2be363574440353
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-07-25 14:44:49 -0700

    installed morgan and re-organized server middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express           = require ('express')
 const bodyParser        = require('body-parser')
 const methodOverride    = require('method-override')
+const logger            = require('morgan')
 const app               = express()
 
 require('./DB/db')
@@ -15,7 +16,10 @@ require('./DB/db')
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
+app.use(logger('dev'))
+app.use()
 
 
 
