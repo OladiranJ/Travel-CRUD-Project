ecd330e4ebbeb485270dfd809a929ae6f2c9e60c
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-26 13:05:30 -0700

    Basic trip index page and new trip page

--- Controllers/TripsController.js
+++ Controllers/TripsController.js
@@ -5,12 +5,16 @@ const Trips = require('../models/trips.js');
 const tripsController = {
     showTripsIndex: async (req, res) => {
         try {
-            console.log('-------------');
-            console.log('trips route');
-            console.log('-------------');
             res.render('Trips-ejs-files/index.ejs');
 
         } catch(err){
+            res.send(err);
+        }
+    },
+    newTripPage: async (req, res) => {
+        try {
+            res.render('Trips-ejs-files/newTripPage.ejs')
+        } catch (err){
             res.send(err);
         }
     }
--- Routes/tripsRoute.js
+++ Routes/tripsRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const tripsController = require('../controllers/TripsController');
 
 router.get('/', tripsController.showTripsIndex);
+
+router.get('/new-trip', tripsController.newTripPage);
 module.exports = router;
