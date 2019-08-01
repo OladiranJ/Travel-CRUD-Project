976219cffc58aedc2b3e7bc05ecbcaba129ff89c
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-08-01 14:28:18 -0700

    add everything back

---
+++ controllers/activitiesController.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+// const Trips = require('../models/trips.js');
+const Activities = require('../models/activities');
+
+const activitiesController = {
+    addActivity: async (req, res) => {
+        try {
+            const activitiesIndex = await Activities.find({});
+            
+            res.render('Trips-ejs-files/createActivity.ejs', {
+                activities: activitiesIndex
+            });
+        }catch(err){
+            res.send(err);
+        }
+    } ,
+    activityPost: async (req, res) => {
+        try{
+            const createdActivity = await Activities.create(req.body);
+            res.redirect('/trips/add-activity');
+
+        } catch(err){
+            res.send(err);
+        }
+    },
+    cityView: async (req, res) => {
+        try{
+            const cityList = await Activities.find( {city: req.params.city});
+            console.log(cityList)
+            res.render('Trips-ejs-files/cityActPage.ejs',{
+                activities: cityList,
+                city: cityList[0].city
+            });
+
+        } catch (err){
+            res.send(err);
+        }
+    },
+    activityDetails: async (req, res) => {
+        try{
+            Activities.findById(req.params.id, (err, findActivity) => {
+                res.render('Trips-ejs-files/actDetailPage.ejs', {
+                    activity: findActivity
+                })
+            }
+            )
+
+        }catch(err){
+            res.send(err);
+        }
+    },
+    deleteActivity: async (req, res) =>{
+        try {
+            Activities.findByIdAndRemove(req.params.id, () =>{
+                res.redirect('/trips/add-activity');
+            })
+
+        }catch(err){
+            res.send(err);
+
+        }
+    }
+}
+
+module.exports = activitiesController;
