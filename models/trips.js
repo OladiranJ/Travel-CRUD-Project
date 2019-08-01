f743708869d16c5e097d4783ebda2d63de0cb8f0
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-08-01 14:31:48 -0700

    s

---
+++ models/trips.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+
+const tripsSchema = new mongoose.Schema({
+    city: {type: String, required: true},
+    country: String,
+    activity: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Activities'
+    }],
+    description: String,
+    date: { type: Date, default: Date.now },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+
+})
+
+const Trip = mongoose.model('Trip', tripsSchema);
+
+module.exports = Trip;
